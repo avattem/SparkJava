237db1d3e051312a8fb5d45a8b6649caaeb8e969
avattem <avattem@miraclesoft.com>
2017-09-30 02:26:24 +0530

    Update UserController.java

--- src/main/java/com/google/appengine/sparkdemo/UserController.java
+++ src/main/java/com/google/appengine/sparkdemo/UserController.java
@@ -23,7 +23,7 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
 
-import com.google.gson.Gson;
+
 
 import spark.ResponseTransformer;
 import spark.Spark;
